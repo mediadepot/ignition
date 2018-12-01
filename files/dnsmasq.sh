24d02280c70c356a0f7885175052cf5e3bbcb344
Jason Kulatunga <jason@thesparktree.com>
2018-12-01 00:53:50 -0800

    installing dnsmasq on the server. removed unnecessary web port on traefik.

---
+++ files/dnsmasq.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+IP_ADDRESS=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
+
+exec /usr/bin/docker run --rm --name $1 -p 53:53/tcp -p 53:53/udp --cap-add=NET_ADMIN andyshinn/dnsmasq:2.75 -S /depot.lan/$(IP_ADDRESS)
