c818b139ae252bd1af08ba5bee42f6ddc2cb1e56
Jason Kulatunga <jason@thesparktree.com>
2018-12-01 10:16:51 -0800

    tweak resolver.

--- files/dnsmasq.sh
+++ files/dnsmasq.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 IP_ADDRESS=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
 
-exec /usr/bin/docker run --rm --name $1 -p 53:53/tcp -p 53:53/udp --cap-add=NET_ADMIN andyshinn/dnsmasq:2.75 \
+exec /usr/bin/docker run --rm --name $1 --network=host --cap-add=NET_ADMIN andyshinn/dnsmasq:2.75 \
+    --log-queries \
     --log-facility=- \
     --bogus-priv \
     --domain-needed \
-    --local-service \
     --no-resolv \
+    --local-service \
+    --local=/depot.lan/ \
     --listen-address=127.0.0.1 \
     --listen-address=${IP_ADDRESS} \
     --address /depot.lan/${IP_ADDRESS}
