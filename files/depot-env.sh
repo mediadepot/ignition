4139b020be1de64014bdf62b8f8084e664060f01
Jason Kulatunga <jason@thesparktree.com>
2018-12-03 21:03:32 -0800

    pass inthe DOCKER_GROUP_ID for netdata.

--- files/depot-env.sh
+++ files/depot-env.sh
@@ -2,7 +2,12 @@
 IP_ADDRESS=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
 
 cat >/etc/sysconfig/mediadepot <<EOL
+# Used by dnsmasq
 DEPOT_SERVER_IP=${IP_ADDRESS}
+
+# Used by netdata
+DOCKER_GROUP_ID=$(id -g docker)
+
 EOL
 
 
